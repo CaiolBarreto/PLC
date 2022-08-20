553bda47ad548ef016ae67c1d5f90c8820b01266
Caio Barreto <caiolondresb@gmail.com>
2022-08-20 12:06:22 -0300

    feature(master): change question 1

--- 1_lista/1_questao.hs
+++ 1_lista/1_questao.hs
@@ -1,5 +1,7 @@
 potentiation :: Int -> Int -> Int
-potentiation n k = n ^ k
+potentiation n 0 = 1
+potentiation n 1 = n
+potentiation n k = n * potentiation n (k - 1)
 
 parseInput str = let [n, k] = map read (words str)
                  in (n, k)
